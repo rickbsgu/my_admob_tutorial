4a02bca68609ddaa25dd7b31d8896a1bcec06195
rickbsgu <rickbsgu@gmail.com>
2022-05-07 21:48:58 -0600

    Works, but needs refactoring

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -2,6 +2,15 @@ import 'dart:io';
 
 class AdHelper {
 
+  static String get openAdUnitId {
+    if (Platform.isAndroid)
+      return 'ca-app-pub-3940256099942544/3419835294';
+    else if (Platform.isIOS)
+      return 'ca-app-pub-3940256099942544/5662855259';
+    else
+      throw UnsupportedError('Unsupported platform');
+  }
+
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/6300978111';
--- lib/home_route.dart
+++ lib/home_route.dart
@@ -15,6 +15,9 @@
 import 'package:admob_ads_in_flutter/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'dart:async';
+import 'package:admob_ads_in_flutter/ad_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeRoute extends StatefulWidget {
   @override
@@ -22,13 +25,94 @@ class HomeRoute extends StatefulWidget {
 }
 
 class _HomeRouteState extends State<HomeRoute> {
+  static const int AD_OPEN_INTERVAL = 2;
+  static const String PLEASE_WAIT_STRING = 'Please wait...';
+  static const String LETS_GET_STARTED_STRING = 'Let\'s get started!';
+  static const String NUM_RUNS_KEY = 'num-invokes';
+  final Future<bool> _theFuture = _initGoogleMobileAds();
+
+  static bool _adsInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    Future.delayed(Duration.zero, () async {
+      _prefs = await SharedPreferences.getInstance();
+      setState(() {
+        _buttonEnabled = !_allowOpenAd;
+      });
+    });
+  }
+
+  static SharedPreferences? _prefs;
+  
+  ///
+  /// beg button enabled state and string control
+  /// 
+  String __buttonString = LETS_GET_STARTED_STRING;
+  bool __buttonEnabled = false;
+
+  bool get _buttonEnabled => __buttonEnabled;
+  String get _buttonString => __buttonString;
+
+  set _buttonEnabled(bool enabled) {
+    __buttonString = enabled ? LETS_GET_STARTED_STRING : PLEASE_WAIT_STRING;
+    __buttonEnabled = enabled;
+  }
+  //
+  // end button enabled state and string control
+  // begin OpenAd control
+  //
+  AppOpenAd? _openAd;
+
+  int get _numRuns => _prefs!.getInt(NUM_RUNS_KEY) ?? 0;
+
+  void _bumpNumRuns() {
+    if (_prefs == null)
+      return;
+
+    int numRuns = _numRuns;
+    if (++numRuns > AD_OPEN_INTERVAL) numRuns = 0;
+    _prefs!.setInt(NUM_RUNS_KEY, numRuns);
+  }
+
+  bool get _allowOpenAd {
+    return _prefs != null && _numRuns == 0;
+  }
+  ///
+  /// end OpenAd control
+  ///
+
+  Future<void> loadOpenAd() async {
+    AppOpenAd.load(
+      adUnitId: AdHelper.openAdUnitId,
+      request: AdRequest(),
+      adLoadCallback: AppOpenAdLoadCallback(onAdLoaded: (ad) {
+        ad.fullScreenContentCallback =
+            FullScreenContentCallback(onAdDismissedFullScreenContent: (_) {
+          setState(() {
+            _buttonEnabled = true;
+          });
+          ad.dispose();
+        });
+
+        _openAd = ad;
+        _openAd!.show();
+      }, onAdFailedToLoad: (err) {
+        print('OpenAd failed to load: ${err.toString()}');
+      }),
+      orientation: AppOpenAd.orientationPortrait,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.primary,
-      body: FutureBuilder<void>(
-        future: _initGoogleMobileAds(),
-        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+      body: FutureBuilder<bool>(
+        future: _theFuture,
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -45,19 +129,34 @@ class _HomeRouteState extends State<HomeRoute> {
                   padding: EdgeInsets.only(bottom: 72),
                 ),
                 if (snapshot.hasData)
-                  ElevatedButton(
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 48.0,
-                        vertical: 12.0,
-                      ),
-                      child: Text('Let\'s get started!'),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Theme.of(context).colorScheme.secondary,
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/game');
+                  Builder(
+                    builder: (BuildContext context) {
+                      print('===> building home route');
+                      if (_allowOpenAd) {
+                        _buttonEnabled = false;
+                        WidgetsBinding.instance
+                            ?.addPostFrameCallback((_) async {
+                          await loadOpenAd();
+                        });
+                      }
+                      _bumpNumRuns();
+                      return ElevatedButton(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 48.0,
+                            vertical: 12.0,
+                          ),
+                          child: Text(_buttonString),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).colorScheme.secondary,
+                        ),
+                        onPressed: _buttonEnabled
+                            ? () {
+                                Navigator.of(context).pushNamed('/game');
+                              }
+                            : null,
+                      );
                     },
                   )
                 else if (snapshot.hasError)
@@ -80,7 +179,35 @@ class _HomeRouteState extends State<HomeRoute> {
     );
   }
 
-  Future<InitializationStatus> _initGoogleMobileAds() {
-    return MobileAds.instance.initialize();
+  static void printInitStatus(InitializationStatus initStatus) {
+    List<MapEntry<String, AdapterStatus>> entries =
+        initStatus.adapterStatuses.entries.toList();
+    for (MapEntry entry in entries)
+      print(' ======> initStatusKey ${entry.key}: ${entry.value.description}');
+  }
+
+  ///
+  /// Prevent multiple inits
+  /// Should only happen on first instantiation of widget
+  /// (we have to wait for it, though, so that's the FutureBuilder's job)
+  /// 
+  static Future<bool> _initGoogleMobileAds() {
+    // FutureBuilder does not like a Future<void>...
+
+    Completer<bool> completer = Completer<bool>();
+    if (!_adsInitialized) {
+      print(' ====> calling initializing ads...');
+      MobileAds.instance.initialize().then((InitializationStatus initStatus) {
+        _adsInitialized = true;
+        print(' ====> ads initialized');
+        printInitStatus(initStatus);
+        completer.complete(true);
+      });
+    } else {
+      print(' ====> repeat initializing call - ignored');
+      completer.complete(true);
+    }
+
+    return completer.future;
   }
 }
